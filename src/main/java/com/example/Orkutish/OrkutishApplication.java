f5ebff5c1cd2c02520fea558cfb084656b412ead
PatrickBeh <93169933+PatrickBeh@users.noreply.github.com>
2025-05-01 21:58:49 -0300

    chore: initialize SpringBoot project with H2/MySQL migration capabilities.

---
+++ src/main/java/com/example/Orkutish/OrkutishApplication.java
@@ -0,0 +1,13 @@
+package com.example.Orkutish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrkutishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrkutishApplication.class, args);
+	}
+
+}
